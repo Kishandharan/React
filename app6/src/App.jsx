e6ec38ed6597f607094cdd6ee0e09247a6b8e44b
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-06-11 12:30:01 +0530

    Props in React

---
+++ app6/src/App.jsx
@@ -0,0 +1,14 @@
+import Calc from "./Calc.jsx";
+
+function App(){
+    return(
+        <>
+            <Calc num1={10} num2={200}/>
+            <Calc num1={20} num2={200}/>
+            <Calc num1={30} num2={200}/>
+        </>
+    );
+}
+
+export default App
+
---
+++ app6/src/Calc.jsx
@@ -0,0 +1,8 @@
+function Calc(props) {
+    var result = props.num1 + props.num2;
+    return (
+        <p>{result}</p>
+    );
+}
+
+export default Calc;
